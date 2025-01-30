0d8ddf2ba062d21bf135a24eec016a870a6843ac
Thennila Mathimaaran <thennil-im19095@stu.kln.ac.lk>
2025-01-30 21:21:25 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import aboutImg from "../assets/images/about.png";
+const About = () => {
+  const info = [
+    //{ text: "Years experience", count: "04" },
+   //{ text: "Completed Projects", count: "04" },
+    //{ text: "Companies Work", count: "01" },
+  ];
+  return (
+    <section id="about" className="py-10 text-white">
+      <div className="text-center mt-8">
+        <h3 className="text-4xl font-semibold">
+          About <span className="text-cyan-600">Me</span>
+        </h3>
+        <p className="text-gray-400 my-3 text-lg"></p>
+        <div className="flex md:flex-row flex-col-reverse items-center md:gap-6 gap-12 px-10 max-w-6xl mx-auto">
+          <div className="p-2">
+            <div className="text-gray-300 my-3">
+              <p className="text-justify leading-7 w-11/12 mx-auto">
+              Hello! I’m Thennila Mathimaaran, a passionate and self-motivated Information Technology graduate seeking a position where I can leverage my expertise in technology, software development, and problem-solving. I am driven to apply my technical skills to contribute to innovative projects, foster business solutions, and drive growth. With a solid foundation in data science, UI/UX designing, and front-end development, I am eager to collaborate with dynamic teams, continuously learn, and make impactful contributions to the technology industry.
+              </p>
+              <div className="flex mt-10 items-center gap-7">
+                {info.map((content) => (
+                  <div key={content.text}>
+                    <h3 className="md:text-4xl text-2xl font-semibold text-white">
+                      {content.count}
+                      <span className="text-cyan-600">+</span>{" "}
+                    </h3>
+                    <span className="md:text-base text-xs">{content.text}</span>
+                  </div>
+                ))}
+              </div>
+              <br />
+              <br />
+              <a href="./src/assets/Thennila Mathimaaran Resume.pdf" download>
+                <button className="btn-primary">Download CV</button>
+              </a>
+            </div>
+          </div>
+          <div className="flex-1 md:mt-0 mt-6 flex justify-center items-center">
+            <div className="lg:w-96 h-full relative sm:w-10/12 w-11/12 max-w-sm aboutImg ">
+              <img
+                src={aboutImg}
+                alt=""
+                className="w-full object-cover bg-cyan-600 rounded-xl"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
